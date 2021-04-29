ffc008e9214c589c1ade601cfeb2af9ba1cf847a
Sureshkumar Subramani <sureshkumar_odc@yahoo.com>
2021-04-29 21:51:38 +0530

    currency select box created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
+import CurrSelect from './../CurrSelect/CurrSelect';
+import currency from '../../data/currency.json';
 import './App.css';
 
 function App() {
   return (<div>
     <div className="flex-column"><h1>Currency Converter</h1></div>
     <div className="flex-row">
-      <div><label>From: <input type="text" /></label></div>
-      <div><label>To: <input type="text" /></label></div>
+      <div><CurrSelect label="From" currencyList={currency.map(item => item.base)}></CurrSelect></div>
+      <div><CurrSelect label="To" currencyList={currency.map(item => item.base)}></CurrSelect></div>
     </div>
   </div>);
 }
